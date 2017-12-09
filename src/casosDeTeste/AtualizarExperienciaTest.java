e02e4f01e9463cf3ddd1401d1c7f22c57754a4f1
gabrielsfirmino <gabrielsfirmino@alu.ufc.br>
2017-12-09 10:48:11 -0300

    Correção de classes e metodos

--- src/casosDeTeste/AtualizarExperienciaTest.java
+++ src/casosDeTeste/AtualizarExperienciaTest.java
@@ -10,11 +10,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import acaoParaTeste.LoginAcao;
 
-public class AdicionarFormacaoTest 
+public class AtualizarExperienciaTest 
 {
 
 	@Test
-	public void TesteAdicionarFormacaoValida() 
+	public void TesteAdicionarExperienciaValida() 
 	{
 		System.setProperty("webdriver.gecko.driver", "/home/gabrielsfirmino/Downloads/geckodriver");
 		
@@ -24,11 +24,21 @@ public class AdicionarFormacaoTest
 		
 		LoginAcao.Logar(drive, "gabrielsfirmino@alu.ufc.br", 85866021);
 		
+		By xpath_1 = By.xpath("//a[@class='btn btn-primary btn-xs']");
+		WebElement button_1 = drive.findElement(xpath_1);
+		button_1.click();
 		
+		By id_1 = By.id("formation_location");
+		WebElement campoInstituicao = drive.findElement(id_1);
+		campoInstituicao.sendKeys("Universidade Federal do Ceara");
+		
+//		By id_1 = By.id("formation_location");
+//		WebElement campoInstituicao = drive.findElement(id_1);
+//		campoInstituicao.sendKeys("Universidade Federal do Ceara");
 	}
 	
 //	@Test
-//	public void TesteAdicionarFormacaoInvalida() 
+//	public void TesteRemoverExperiencia() 
 //	{
 //		fail("Not yet implemented");
 //	}
