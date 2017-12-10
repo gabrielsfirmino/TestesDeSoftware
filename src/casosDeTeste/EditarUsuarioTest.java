9fa917ef1a1792ccfabc05ace0e69520924806ad
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-12-10 12:48:24 -0300

    Finalizado teste Editar Usuário com informações inválidas

--- src/casosDeTeste/EditarUsuarioTest.java
+++ src/casosDeTeste/EditarUsuarioTest.java
@@ -60,4 +60,48 @@ public class EditarUsuarioTest {
 		
 		assertTrue((birthplace.isEmpty() && crm.isEmpty() && cep.isEmpty() && number.isEmpty()));
 	}
+	
+	@Test
+	public void TesteEditarUsuarioDadosInvalidos() {
+		File file = new File("C:\\Users\\CESAR\\Documents\\Github\\TrabFinalTestes\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+		
+		WebDriver drive = new FirefoxDriver();
+		
+		drive.get("http://med-profile.apps.intmed.com.br/");
+		
+		LoginAcao.Logar(drive, "gabrielsfirmino@alu.ufc.br", 85866021);
+		
+		By idUserBirthplace = By.id("user_birthplace");
+		WebElement userBirthplace = drive.findElement(idUserBirthplace);
+		userBirthplace.clear();
+		userBirthplace.sendKeys("sadgsadgyasdgaydgaydgays");
+		
+		By idUserCRM = By.id("user_crm");
+		WebElement userCRM = drive.findElement(idUserCRM);
+		userCRM.clear();
+		userCRM.sendKeys("437546545645456456");
+		
+		By idUserCRMLocation = By.id("user_crm_location");
+		WebElement userCRMLocation = drive.findElement(idUserCRMLocation);
+		userCRMLocation.sendKeys("XR");
+		
+		By idUserCEP = By.id("user_address_attributes_cep");
+		WebElement userCEP = drive.findElement(idUserCEP);
+		userCEP.clear();
+		userCEP.sendKeys("999999999");
+		
+		By idAddressNumber = By.id("user_address_attributes_number");
+		WebElement addressNumber = drive.findElement(idAddressNumber);
+		addressNumber.clear();
+		addressNumber.sendKeys("450547878787784487");
+		
+		By xpathSave = By.xpath("//*[@id=\"edit_user_440\"]/div[2]/button");
+		WebElement save = drive.findElement(xpathSave);
+		save.click();
+		
+		By xpathError = By.xpath("/html/body/div");
+		WebElement error = drive.findElement(xpathError);
+		assertTrue(error.isDisplayed());
+	}
 }
