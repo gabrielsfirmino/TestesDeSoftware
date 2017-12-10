3de8a89180b9039a78efddaf52f6d8a7e8154de7
gabrielsfirmino <gabrielsfirmino@alu.ufc.br>
2017-12-10 12:30:55 -0300

    Casos de teste para o login

--- src/casosDeTeste/AtualizarExperienciaTest.java
+++ src/casosDeTeste/AtualizarExperienciaTest.java
@@ -7,14 +7,57 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import acaoParaTeste.LoginAcao;
 
 public class AtualizarExperienciaTest 
 {
 
+//	@Test
+//	public void TesteAdicionarExperiencia() 
+//	{
+//		System.setProperty("webdriver.gecko.driver", "/home/gabrielsfirmino/Downloads/geckodriver");
+//		
+//		WebDriver drive = new FirefoxDriver();
+//		
+//		drive.get("http://med-profile.apps.intmed.com.br/");
+//		
+//		LoginAcao.Logar(drive, "gabrielsfirmino@alu.ufc.br", 85866021);
+//		
+//		By xpath_1 = By.xpath("//a[@href='/experiences/new']");
+//		WebElement button_1 = drive.findElement(xpath_1);
+//		button_1.click();
+//		
+//		By id_1 = By.id("experience_post");
+//		WebElement campoCargo = drive.findElement(id_1);
+//		campoCargo.sendKeys("Testador");
+//		
+//		By id_2 = By.id("experience_location");
+//		WebElement campoInstituicao = drive.findElement(id_2);
+//		campoInstituicao.sendKeys("Universidade Federal do Ceara");
+//		
+//		By id_3 = By.id("experience_initial_date");
+//		WebElement campoDataInicial = drive.findElement(id_3);
+//		campoDataInicial.sendKeys("032017");
+//		
+//		By id_4 = By.id("experience_final_date");
+//		WebElement campoDataFinal = drive.findElement(id_4);
+//		campoDataFinal.sendKeys("012018");
+//		
+//		By xpath_2 = By.xpath("//button[@class='btn btn-info pull-right']");
+//		WebElement button_2 = drive.findElement(xpath_2);
+//		button_2.click();
+//		
+//		By xpath_3 = By.xpath("//div[@id='experiences']");
+//		WebElement elemento = drive.findElement(xpath_3);
+//		assertTrue(elemento.getText().contains("Testador"));
+//		
+//		drive.close();
+//	}
+	
 	@Test
-	public void TesteAdicionarExperienciaValida() 
+	public void TesteRemoverExperiencia() 
 	{
 		System.setProperty("webdriver.gecko.driver", "/home/gabrielsfirmino/Downloads/geckodriver");
 		
@@ -24,23 +67,19 @@ public class AtualizarExperienciaTest
 		
 		LoginAcao.Logar(drive, "gabrielsfirmino@alu.ufc.br", 85866021);
 		
-		By xpath_1 = By.xpath("//a[@class='btn btn-primary btn-xs']");
+		By xpath_1 = By.xpath("/html/body/div/div[2]/section/div/div/div[6]/div[2]/table/tbody/tr[1]/td[7]/div/a[2]");
 		WebElement button_1 = drive.findElement(xpath_1);
 		button_1.click();
 		
-		By id_1 = By.id("formation_location");
-		WebElement campoInstituicao = drive.findElement(id_1);
-		campoInstituicao.sendKeys("Universidade Federal do Ceara");
+		By xpath_2 = By.xpath("/html/body/div/div[2]/section/div/div/div[6]/div[2]/table/tbody/tr[1]/td[7]/div/a[2]");
+		WebElement button_2 = drive.findElement(xpath_2);
+		button_2.click();
 		
-//		By id_1 = By.id("formation_location");
-//		WebElement campoInstituicao = drive.findElement(id_1);
-//		campoInstituicao.sendKeys("Universidade Federal do Ceara");
+		WebDriverWait wait = new WebDriverWait(drive, 10);
+		
+		By xpath_3 = By.xpath("//div[@id='experiences']");
+		WebElement elemento = drive.findElement(xpath_3);
+		assertFalse(elemento.getText().contains("Testador"));
 	}
-	
-//	@Test
-//	public void TesteRemoverExperiencia() 
-//	{
-//		fail("Not yet implemented");
-//	}
 
 }
---
+++ src/casosDeTeste/LoginTest.java
@@ -0,0 +1,21 @@
+package casosDeTeste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void TesteLogarComUsuarioCadastrado() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void TesteLogarComUsuarioNaoCadastrado() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
